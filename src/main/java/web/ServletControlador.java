c353c4c55f658f4a97145561224846da395b30a1
Matias <matiasalvarez22@gmail.com>
2022-02-15 17:38:27 -0300

    Metodo Insertar

--- src/main/java/web/ServletControlador.java
+++ src/main/java/web/ServletControlador.java
@@ -5,15 +5,21 @@ import dominio.Cliente;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/ServletControlador")
-public class ServletControlador extends HttpServlet{
-    
+public class ServletControlador extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.accionDefault(request, response);
+    }
+
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             List<Cliente> clientes = new ClienteDaoJDBC().listar();
             System.out.println("clientes = " + clientes);
@@ -25,13 +31,60 @@ public class ServletControlador extends HttpServlet{
             ex.printStackTrace(System.out);
         }
     }
-    
-    private double calcularSaldoTotal(List<Cliente> clientes){
+
+    private double calcularSaldoTotal(List<Cliente> clientes) {
         double saldoTotal = 0;
-        
-        for(Cliente cliente : clientes){
+
+        for (Cliente cliente : clientes) {
             saldoTotal += cliente.getSaldo();
         }
         return saldoTotal;
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String accion = request.getParameter("accion");
+
+        if (accion != null) {
+            switch (accion) {
+                case "insertar":
+                {
+                    try {
+                        this.insertarCliente(request, response);
+                    } catch (SQLException ex) {
+                        ex.printStackTrace(System.out);
+                    }
+                }
+                    break;
+
+                default: this.accionDefault(request, response);
+            }
+        }else{
+            this.accionDefault(request, response);
+        }
+    }
+
+    private void insertarCliente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        //Recuperamos los valores del formulario agregarCliente
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+        double saldo = 0;
+        String saldoString = request.getParameter("saldo");
+        if(saldoString != null && !"".equals(saldoString)){
+            saldo = Double.parseDouble(saldoString);
+        }
+        
+        //Creamos el objeto de cliente (modelo)
+        Cliente cliente = new Cliente(nombre, apellido, email, telefono, saldo);
+        
+        //Insertamos el objeto en la Base de Datos
+        int registrosModificados = new ClienteDaoJDBC().insertar(cliente);
+        System.out.println("registrosModificados = " + registrosModificados);
+
+        //Redirijimos a la accion por default
+        this.accionDefault(request, response);
+    }
+
 }
